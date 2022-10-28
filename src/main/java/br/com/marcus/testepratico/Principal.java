7bc87c06b6d7c34dcc272dbbedb678f57e239a87
Marcus <marcus10121@hotmail.com>
2022-10-28 01:23:10 -0300

    :sparkles: feat: Requisito[3.2] Removendo funcionário João da lista.

--- src/main/java/br/com/marcus/testepratico/Principal.java
+++ src/main/java/br/com/marcus/testepratico/Principal.java
@@ -18,13 +18,18 @@ public class Principal {
      * acima.
      */
     adicionaAoArray();
+
+    /**
+     * 3.2 – Remover o funcionário “João” da lista.
+     */
+    removeByName("João");
   }
 
   public static void adicionaAoArray() throws InvalidDateFormatException, FutureDateException {
     System.out
         .println("[Requisito 3.1] ___________________________________________________________");
     System.out.println(
-        "========+========== Iniciando insersão de todos os funcionários ======+============ \n");
+        "========+========== Iniciando insersão de todos os funcionários ======+============");
 
     /** Array de nomes (Seguindo ordem da tabela). */
     String[] nomes = { "Maria", "João", "Caio", "Miguel", "Alice", "Heitor", "Arthur", "Laura",
@@ -50,17 +55,46 @@ public class Principal {
         Funcionario funcionario = new Funcionario(nomes[index], datas[index], salarios[index],
             funcoes[index]);
         funcionarios.add(funcionario);
-        System.out.println(funcionario.toString());
       }
 
       System.out.println(
-          "\n=============================== Insersão Concluida! ===============================");
+          "=============================== Insersão Concluida! ===============================\n");
 
     } catch (Exception e) {
       System.out.println(
-          "=============================== A Insersão  Falhou! ===============================");
+          "=============================== A Insersão Falhou! ===============================\n");
       System.out.println(e);
     }
 
+  }
+
+  public static void removeByName(String nome) {
+    System.out
+        .println("[Requisito 3.2] ___________________________________________________________");
+    System.out.println(
+        "========+========== Removendo funcionário da lista. ======+============");
+    System.out.printf("==== Lista de funcionários [%s]: \n\n", funcionarios.size());
+    for (Funcionario fun : funcionarios) {
+      System.out.println(fun.toString());
+    }
+    Funcionario funcionarioRemove = null;
+    System.out.printf("\n___ Verificando se o funcionário de nome: %s, se encontra na lista. ___\n", nome);
+    for (Funcionario fun : funcionarios) {
+      if (fun.getNome().equals(nome)) {
+        funcionarioRemove = fun;
+        System.out.printf(">> Funcionário de nome: %s, encontrado. iniciando remoção.\n", nome);
+      }
+    }
+
+    if (funcionarioRemove.equals(null)) {
+      System.out.println(">> Funcionário não encontrado.");
+    } else {
+      funcionarios.remove(funcionarioRemove);
+      System.out.printf(">> Funcionário de nome: %s, removido com sucesso!\n", nome);
+      System.out.printf("==== Lista de funcionários atualizada [%s]: \n\n", funcionarios.size());
+      for (Funcionario fun : funcionarios) {
+        System.out.println(fun.toString());
+      }
+    }
   }
 }
